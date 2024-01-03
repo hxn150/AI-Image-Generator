de07cf7b9a717647fb860a58a7555ce31690ea89
Helen Nguyen <hxn150@case.edu>
2024-01-03 15:41:07 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ImageGenerator from './components/ImageGenerator/ImageGenerator';
+
+function App() {
+  return (
+    <div>
+      <ImageGenerator/>
+    </div>
+  );
+}
+
+export default App;
